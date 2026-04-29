f45ab4ce4cfd45ca9b0f703b9cb70457b1dcb0b2
JaGaLope <jakelopez660@gmail.com>
2026-04-29 15:41:30 -0700

    Merge branch 'main' of https://github.com/JaGaLopez/NBAautoreport
    cleaup

--- analytics/GetTeamStats.py
+++ analytics/GetTeamStats.py
@@ -1,16 +1,9 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import sklearn as sk
 from nba_api.stats.endpoints import leaguedashteamstats as teamstats
-from scipy.stats import percentileofscore
 
-def CompareAllTeams(SEASON):
-
+def GetTeamStats(SEASON):
     SEASON_TYPE = "Regular Season"
 
-    
     df = teamstats.LeagueDashTeamStats(
         season=SEASON,
         season_type_all_star=SEASON_TYPE,
