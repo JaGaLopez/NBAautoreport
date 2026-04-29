e64d1c0d9822eb139598ace9e69dc3bc7b477488
JaGaLope <jakelopez660@gmail.com>
2026-04-29 15:52:35 -0700

    new api endpoint

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from analytics.BuildAverageTeam import BuildAverageTeam
+from analytics.GetTeamStats import GetTeamStats
 
 app = FastAPI()
 
@@ -7,3 +8,8 @@ app = FastAPI()
 def average_team(season: str):
     data = BuildAverageTeam(season)
     return data.to_dict()
+
+@app.get("/teams/{season}")
+def teams(season: str):
+    data = GetTeamStats(season)
+    return data.to_dict(orient="records")
