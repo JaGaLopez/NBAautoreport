8b71996701a90cad068cbee06789c49d53571aee
JaGaLopez <122402207+JaGaLopez@users.noreply.github.com>
2026-04-29 14:13:09 -0700

    Delete api/api.py

--- api/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from analytics.BuildAverageTeam import BuildAverageTeam
-
-app = FastAPI()
-
-@app.get("/average-team/{season}")
-def average_team(season: str):
-    data = BuildAverageTeam(season)
-    return data.to_dict()
